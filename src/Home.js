79513fa34c9af6c76b66851b970f4c4cf245d18e
collettshuma <collettshuma@gmail.com>
2022-07-30 21:19:32 +0200

    update Home

--- src/Home.js
+++ src/Home.js
@@ -1,62 +1,22 @@
 import { useState } from "react";
 
 const Home = () => {
-    const handleClick = () => {console.log('sup fellas');}
-    const handleClickAgain = (name) => {console.log('sup ' + name);}
-
-    const [name,setName] = useState('Dav');
-    const [age,setAge]= useState(25);
-    const handle = () => {
-        setName('shaz');
-        setAge(23);
-    }
+    const [blogs,setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ]);
 
     return (  
-        <div className="Home">
-            <h1>Homepage</h1>
-            <p>{ name } is { age } old</p>
-            <button onClick={handleClick}>Click</button>
-            <button onClick={() => {handleClickAgain('jay')}}>ClickMe</button>
-            <button onClick={handle}>ClickMeAgain</button>
+        <div className="home">
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={ blog.id }>
+                    <h2>{ blog.title }</h2>
+                    <p>Written byt { blog.author}</p>
+                </div>
+            ))}
         </div>
     );
 }
  
 export default Home;
-
-/*const Home = () => {
-    const handleClickAgain = (name) => {console.log('hello ' + name);}
-    const handlestick = () => {console.log('hello there')};
-
-    return (  
-        <div className="Home">
-           <h1>Homepage</h1>
-           <button onClick={handlestick}>ClickMe</button>
-           <button onClick={() => {handleClickAgain('mario')}}>ClickMeAgain</button>
-        </div>
-    );
-}
- 
-export default Home;
-
-
-
-/*import { useState } from "react";
-
-const Home = () => {
-    const [name,setName] = useState('mario');
-
-    const handleClick = () => {setName('luigi')};
-
-
-
-    return (  
-        <div className="Home">
-            <h2>Homepage</h2>
-            <p>{ name }</p>
-            <button onClick={handleClick}>ClickMe</button>
-        </div>
-    );
-}
- 
-export default Home;*/
